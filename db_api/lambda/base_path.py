3b9867aa928ac289e41f2598fb24a4bc76e3d136
timlac <tim@lachmann.nu>
2023-08-09 15:14:40 +0200

    removed aws-cdk-code (moved to other repo)

--- db_api/lambda/base_path.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def handler(event, context):
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Welcome to my API"}),
-    }
-    return response
